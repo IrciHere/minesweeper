3541db3bde308979354e2ac64061c9479d388bd8
TheDawgry <ircurlicus123@gmail.com>
2019-01-26 00:36:31 +0100

    Poprawienie funkcjonalności silnika
    Puste pola obok siebie odkrywają się automatycznie

--- mainWindow.cs
+++ mainWindow.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,7 +18,7 @@ namespace minesweeper
         public MainWindow()
         {
             //tworzenie okna
-            this.Size = new System.Drawing.Size(305, 395);
+            this.Size = new Size(305, 395);
             this.Text = "Minesweeper";
 
             //siatka pola
@@ -28,8 +29,8 @@ namespace minesweeper
             //przycisk uruchamiający
             startButton = new Button
             {
-                Size = new System.Drawing.Size(100, 50),
-                Location = new System.Drawing.Point(95, 25),
+                Size = new Size(100, 50),
+                Location = new Point(95, 25),
                 Text = "START"
             };
             startButton.MouseClick += StartButton_MouseClick;
@@ -43,7 +44,7 @@ namespace minesweeper
             startButton.Enabled = false;
 
             //tworzy wzór pola minowego
-            MineFieldGenerator generator = new MineFieldGenerator(width, height, howManyBombs);
+            var generator = new MineFieldGenerator(width, height, howManyBombs);
 
             //rysuje pole na podstawie wygenerowanego wzoru
             fieldArray = new Field[width + 2, height + 2];
@@ -51,7 +52,7 @@ namespace minesweeper
             {
                 for (int j = 1; j < height + 1; j++)
                 {
-                    fieldArray[i, j] = new Field(this, positionX, positionY, generator.allFieldBool[i, j], generator.allFieldInt[i, j]);
+                    fieldArray[i, j] = new Field(this, positionX, positionY, generator.allFieldBool[i, j], generator.allFieldInt[i, j], i, j);
                     positionX += 25;
                 }
                 positionY += 25; positionX = 30;
@@ -59,6 +60,7 @@ namespace minesweeper
             positionX = 30; positionY = 30;
         }
 
+        //metoda sprawdzająca wygraną
         public void CheckWin()
         {
             int fields = width * height;
@@ -74,6 +76,44 @@ namespace minesweeper
             {
                 MessageBox.Show("Congratulations, you won!");
                 Application.Exit();
+            }
+        }
+
+        //metoda wywoływana gdy kliknięto puste pole - otwiera też pola dookoła
+        public void CheckNearFields(int fieldX, int fieldY)
+        {
+            int startX = (fieldX - 1), startY = (fieldY - 1);
+
+            for (int i=startX; i < startX + 3; i++)
+            {
+                for (int j = startY; j < startY + 3; j++)
+                {
+                    //wszystkie warunki kiedy nie wykonywać funkcji zawarłem w jednym boolu
+                    bool v = (i == fieldX && j == fieldY) || i == 0 || j == 0 || i == (width + 1) || j == (height + 1);
+
+                    if (!v) //wnętrze tego ifa jest skopiowane z klasy Field
+                    {
+                        if (!fieldArray[i, j].isClicked)
+                        {
+                            if (fieldArray[i, j].bombsNearby != 0)
+                            {
+                                fieldArray[i, j].fieldButton.Text = fieldArray[i, j].bombsNearby.ToString();
+                            }
+
+                            fieldArray[i, j].fieldButton.Enabled = false;
+                            fieldArray[i, j].isClicked = true;
+                            fieldArray[i, j].fieldButton.BackColor = Color.FromArgb(255, 204, 204);
+                            CheckWin();
+                            
+                            //jeżeli na którymś z okolicznych pól nie ma bomby, sprawdza również to pole
+                            if (fieldArray[i, j].bombsNearby == 0)
+                            {
+                                CheckNearFields(i, j);
+                            }
+                        }
+                
+                    }             
+                }
             }
         }
     }
