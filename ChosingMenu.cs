9ce1377afaeee822528aec0a1a46c81f77bda58e
TheDawgry <ircurlicus123@gmail.com>
2019-02-04 03:12:44 +0100

    Custom minefield
    Added custom minefield generator

--- ChosingMenu.cs
+++ ChosingMenu.cs
@@ -6,7 +6,8 @@ namespace minesweeper
 {
     public class ChosingMenu : Form
     {
-
+        public TextBox widthBox, heightBox, minesBox;
+        public Label labelEasy, labelMedium, labelHard, widthLabel, heightLabel, minesLabel;
         public Button buttonEasy, buttonMedium, buttonHard, buttonCustom;
         public Label label;
         public ChosingMenu()
@@ -14,6 +15,7 @@ namespace minesweeper
             Size = new Size(300, 365);
             Text = "Choose Difficulty Level";
 
+            //EASY DIFFICULTY
             buttonEasy = new Button
             {
                 Size = new Size(100, 50),
@@ -23,6 +25,17 @@ namespace minesweeper
             };
             buttonEasy.MouseClick += ButtonEasy_MouseClick;
 
+            labelEasy = new Label
+            {
+                Size = new Size(100, 50),
+                Location = new Point(150, 35),
+                Text = "9x9 field,\n10 bombs",
+                Font = new Font("Arial", 10)
+            };
+            Controls.Add(labelEasy);
+
+
+            //MEDIUM DIFFICULTY
             buttonMedium = new Button
             {
                 Size = new Size(100, 50),
@@ -32,6 +45,16 @@ namespace minesweeper
             };
             buttonMedium.MouseClick += ButtonMedium_MouseClick;
 
+            labelMedium = new Label
+            {
+                Size = new Size(100, 50),
+                Location = new Point(150, 110),
+                Text = "16x16 field,\n40 bombs",
+                Font = new Font("Arial", 10)
+            };
+            Controls.Add(labelMedium);
+
+            //HARD DIFFICULTY
             buttonHard = new Button
             {
                 Size = new Size(100, 50),
@@ -41,18 +64,76 @@ namespace minesweeper
             };
             buttonHard.MouseClick += ButtonHard_MouseClick;
 
+            labelHard = new Label
+            {
+                Size = new Size(100, 50),
+                Location = new Point(150, 185),
+                Text = "30x16 field,\n99 bombs",
+                Font = new Font("Arial", 10)
+            };
+            Controls.Add(labelHard);
+
+            //CUSTOM DIFFICULTY
             buttonCustom = new Button
             {
                 Size = new Size(100, 50),
                 Location = new Point(25, 250),
                 Text = "CUSTOM",
                 Parent = this,
-                Enabled = false
+                //Enabled = false
             };
             buttonCustom.MouseClick += ButtonCustom_MouseClick;
 
+                //set width
+            widthBox = new TextBox                              
+            {
+                Size = new Size(30, 15),
+                Location = new Point(175, 235),
+                MaxLength = 3,
+            };
+            Controls.Add(widthBox);
+
+            widthLabel = new Label
+            {
+                Location = new Point(135, 237),
+                Text = "Width:"
+            };
+            Controls.Add(widthLabel);
+
+                //set height
+            heightBox = new TextBox
+            {
+                Size = new Size(30, 15),
+                Location = new Point(175, 260),
+                MaxLength = 3,
+            };
+            Controls.Add(heightBox);
+
+            heightLabel = new Label
+            {
+                Location = new Point(135, 262),
+                Text = "Height:"
+            };
+            Controls.Add(heightLabel);
+
+                //set mines
+            minesBox = new TextBox
+            {
+                Size = new Size(30, 15),
+                Location = new Point(175, 285),
+                MaxLength = 3,
+            };
+            Controls.Add(minesBox);
+
+            minesLabel = new Label
+            {
+                Location = new Point(135, 287),
+                Text = "Mines:"
+            };
+            Controls.Add(minesLabel);
         }
 
+        //METHODS USED AFTER PRESSING BUTTONS
         private void ButtonEasy_MouseClick(object sender, MouseEventArgs e)
         {
             MainWindow mainWindow = new MainWindow(this, 9, 9, 10);
@@ -74,9 +155,37 @@ namespace minesweeper
             mainWindow.ShowDialog();
         }
 
+        //SECTION FOR CUSTOM FIELD GENERATOR
         private void ButtonCustom_MouseClick(object sender, MouseEventArgs e)
         {
-            throw new NotImplementedException();
+            if (CheckConditions())
+            {
+                int customWidth, customHeight, customMines;
+                customWidth = int.Parse(widthBox.Text); customHeight = int.Parse(heightBox.Text); customMines = int.Parse(minesBox.Text);
+
+                MainWindow mainWindow = new MainWindow(this, customWidth, customHeight, customMines);
+                Visible = false;
+                mainWindow.ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show("NIEPRAWIDŁOWE WARTOŚCI");
+            }
+        }
+
+        private bool CheckConditions()
+        {
+            if (Int32.TryParse(widthBox.Text, out int width) && Int32.TryParse(heightBox.Text, out int height) && Int32.TryParse(widthBox.Text, out int mines))
+            {
+                if(width <= 100 && width > 0 && height <= 100 && height > 0 && mines > 0)
+                {
+                    if (mines < width * height)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
         }
     }
 }
